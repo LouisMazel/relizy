38425bdc806b1e09d9a84119ec7d6aa8810d5b85
LouisMazel <me@loicmazuel.com>
2025-11-27 23:27:05 +0100

    feat(docs): add contributors section

--- docs/typedoc.config.js
+++ docs/typedoc.config.js
@@ -1,6 +1,7 @@
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
 export default {
   entryPoints: ['./../src/index.ts'],
+  exclude: ['./../src/**/*.spec.ts'],
   plugin: ['typedoc-plugin-markdown', 'typedoc-vitepress-theme'],
   out: './src/typedoc',
   docsRoot: './src',
