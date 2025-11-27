b5a2a7071531702bf7e3d71161d501b94b657f37
LouisMazel <me@loicmazuel.com>
2025-11-27 23:27:05 +0100

    feat(docs): add contributors section

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,20 +1,40 @@
+import type { MazUiOptions } from 'maz-ui/plugins'
 import type { Theme } from 'vitepress'
 import VueTermynalPlugin from '@lehoczky/vue-termynal'
+import { MazUi } from 'maz-ui/plugins'
 import DefaultTheme from 'vitepress/theme'
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
-import RelizyTerminal from './components/RelizyTerminal.vue'
+import Layout from './components/Layout.vue'
+import 'maz-ui/styles'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: () => h(Layout),
   enhanceApp({ app }) {
     app.use(VueTermynalPlugin)
-    app.component('RelizyTerminal', RelizyTerminal)
+
+    app.use(MazUi, {
+      theme: {
+        darkModeStrategy: 'class',
+        strategy: 'hybrid',
+        overrides: {
+          foundation: {
+            'border-width': '1px',
+          },
+          colors: {
+            light: {
+              primary: '272 99% 54%',
+              secondary: '210 100% 56%',
+            },
+            dark: {
+              primary: '272 99% 54%',
+              secondary: '210 100% 56%',
+            },
+          },
+        },
+      },
+    } satisfies MazUiOptions)
   },
 } satisfies Theme
